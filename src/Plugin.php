fa26adc39f6eae9dcee80c7c37de09da24f63827
Zeepaardje <rvaken98@gmail.com>
2025-11-12 18:01:19 +0100

    integrated own work on laravel testing plugin into the new pest plugin version

--- src/Plugin.php
+++ src/Plugin.php
@@ -25,8 +25,34 @@ final class Plugin implements Bootable
         }
 
         // Automatically extend tests in 'Integration' directory with IntegrationTestCase
-        pest()->extend(IntegrationTestCase::class)
-            ->use(DatabaseTruncation::class)
-            ->in('Integration');
+        // We need to use absolute paths because pest() is called from Plugin::boot()
+        // which means Backtrace::file() returns Plugin.php, not the test file
+        $cwd = getcwd();
+        $testsPath = $cwd . DIRECTORY_SEPARATOR . 'tests';
+        $integrationPath = $testsPath . DIRECTORY_SEPARATOR . 'Integration';
+        
+        // Try to find the tests directory (could be 'tests' or 'Tests')
+        if (!is_dir($testsPath)) {
+            $testsPath = $cwd . DIRECTORY_SEPARATOR . 'Tests';
+            $integrationPath = $testsPath . DIRECTORY_SEPARATOR . 'Integration';
+        }
+        
+        if (function_exists('error_log')) {
+            error_log('[PestPluginIntegrationTests] CWD: ' . $cwd);
+            error_log('[PestPluginIntegrationTests] Tests path: ' . $testsPath);
+            error_log('[PestPluginIntegrationTests] Integration path: ' . $integrationPath);
+            error_log('[PestPluginIntegrationTests] Integration exists: ' . (is_dir($integrationPath) ? 'yes' : 'no'));
+        }
+        
+        // Use absolute path - pass the directory, ->in() will handle finding files
+        if (is_dir($integrationPath)) {
+            $absolutePath = realpath($integrationPath);
+            if ($absolutePath) {
+                // Pass the absolute directory path - UsesCall will resolve it correctly
+                pest()->extend(IntegrationTestCase::class)
+                    ->use(DatabaseTruncation::class)
+                    ->in($absolutePath);
+            }
+        }
     }
 }
